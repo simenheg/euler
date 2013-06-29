f66575f763c58e9a1c29e12caf74d603bf212feb
Simen Heggestøyl <simenheg@ifi.uio.no>
2013-06-29 12:07:40 +0200

    Renamed directories

--- 10/problem10.m
+++
@@ -1,8 +0,0 @@
-## The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
-
-## Find the sum of all the primes below two million.
-
-format long
-sum (primes (2000000))
-
-## => 142913828922
--- 16/problem16.lisp
+++
@@ -1,9 +0,0 @@
-;; 2¹⁵ = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
-
-;; What is the sum of the digits of the number 2¹⁰⁰⁰?
-
-(defun digit-sum (n)
-  (loop for c across (write-to-string n)
-        sum (parse-integer (string c))))
-
-(digit-sum (expt 2 1000)) ; => 1366
